bdcab413cafe97fef0bdf050edf38f81599055e6
Drew Stinnett <drew.stinnett@duke.edu>
2016-01-17 14:24:47 -0500

    Adding in some better badge display

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ require 'destiny'
 class WelcomeController < ApplicationController
 
   def index
+
     ## TODO:  Be smarter about which clan to display on the front page
     ## For now, just use the first clan in the db
     @clan = Clan.first
@@ -12,6 +13,11 @@ class WelcomeController < ApplicationController
         @characters << character
       end
     end
+
+    @characters_sorted = @characters.sort_by{|c| c.api_info['data']['characterBase']['powerLevel']}
+    @characters_sorted.reverse!
+    @characters_sorted = @characters_sorted.take(10)
+
   end
 
 end
